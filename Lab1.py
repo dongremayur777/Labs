14e65ed18a0d88c404dba7efea63284f631131c2
dongremayur777 <59500283+dongremayur777@users.noreply.github.com>
2020-01-26 00:40:31 +0530

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,24 @@
+print("Helllo World")
+for x in range(2,10,1):
+    print("Mayur Dongre")
+
+y=input('Enter a Number')    
+if(int(y)%2==0):
+    print(y)
+      
+print(7/8)
+
+print("Hello Mayur Here")      
+
+z=["Mayur","Vinit","Yash","Nischay",7]
+print(z)
+
+q=print("Mayur","Vinit","Yash","Nischay")
+print(q)
+
+
+def printme( str ):   
+   print (str)
+   return
+
+printme("Mayur is Student Of IIIT")
